37526f8248991e1190f83067ee2199273cad436c
jsk0821@kaist.ac.kr <jsk0821@kaist.ac.kr>
2022-01-08 15:43:18 +0900

    implement progressbar

--- app/src/main/java/com/example/tournament/choose_one.java
+++ app/src/main/java/com/example/tournament/choose_one.java
@@ -2,13 +2,16 @@ package com.example.tournament;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -24,6 +27,8 @@ public class choose_one extends Fragment {
     ArrayList<Integer> BitImages;
     private ImageView image_first;
     private ImageView image_second;
+    private ProgressBar progressBar;
+    int cur_progress;
     int first,second;
     Bundle bundle;
 
@@ -36,29 +41,44 @@ public class choose_one extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.choose_one, container,false);
+        context = getContext();
+        progressBar = new ProgressBar(context);
+
+        progressBar = root.findViewById(R.id.progressBar);
+        progressBar.setMax(80);
+        progressBar.setProgressTintList(ColorStateList.valueOf(Color.RED));
 
         BitImages = new ArrayList<>();
+
         image_first = root.findViewById(R.id.imageView_first);
         image_second = root.findViewById(R.id.imageView_second);
         order = getArguments().getInt("order");
         BitImages = getArguments().getIntegerArrayList("BitImages");
 
         if(order<=7){
+            cur_progress=0;
+            progressBar.setProgress(cur_progress);
             inorder = order;
             first = inorder;
             second = inorder+1;
         }
         else if(order<=11){
+            cur_progress=25;
+            progressBar.setProgress(cur_progress);
             inorder = order-8;
             first = inorder;
             second = inorder+1;
         }
         else if(order<=13){
+            cur_progress=50;
+            progressBar.setProgress(cur_progress);
             inorder = order-12;
             first = inorder;
             second = inorder+1;
         }
         else{
+            cur_progress=75;
+            progressBar.setProgress(cur_progress);
             inorder = order-14;
             first = inorder;
             second = inorder+1;
@@ -70,9 +90,10 @@ public class choose_one extends Fragment {
         Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(), BitImages.get(second));
         image_second.setImageBitmap(bitmap2);
 
-        context = getContext();
+
 
         bundle = new Bundle();
+
 
         image_first.setOnClickListener(new View.OnClickListener() {
             @Override
